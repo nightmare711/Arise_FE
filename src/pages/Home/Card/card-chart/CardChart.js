27906661b0dd7b0da501fd0e44e047e4444b79c7
Night mare <hnbtranspkt@gmail.com>
2021-03-17 19:58:54 +0700

    add card chart

---
+++ src/pages/Home/Card/card-chart/CardChart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const CardChart = () => {
+	return (
+		<div className='card card-chart'>
+			<span className='title-primary'>BBOO Price Chart</span>
+		</div>
+	)
+}
