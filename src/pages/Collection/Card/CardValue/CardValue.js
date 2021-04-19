4e2f399f3697c360a1dfd594b9f22718ca23ec4d
Night mare <hnbtranspkt@gmail.com>
2021-04-19 19:53:08 +0700

    Autoplay on mobile

--- src/pages/Collection/Card/CardValue/CardValue.js
+++ src/pages/Collection/Card/CardValue/CardValue.js
@@ -4,16 +4,13 @@ import { Player } from 'video-react'
 import './CardValue.css'
 
 export const CardValue = ({ title, image, scarcity, video }) => {
-	function onMouseLeftDown(event) {
-		if (event.button === 2) {
-		}
-	}
 	return (
-		<div onMouseDown={onMouseLeftDown} className='card-value-collection'>
+		<div className='card-value-collection'>
 			<Player
 				// fluid={false}
 				// height={500}
 				// width={300}
+				playsInline
 				aspectRatio='4:3'
 				loop
 				muted
