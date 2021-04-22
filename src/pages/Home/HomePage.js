bee0501ac1887847e639e71cebc2a85171d4e1d8
Night mare <hnbtranspkt@gmail.com>
2021-04-22 11:35:59 +0700

    Drop banner

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,17 +1,8 @@
 import React from 'react'
-import {
-	CardIntro,
-	CardInfo,
-	CardSecureInfo,
-	CardRoadmap,
-	CardExtendInfo,
-	CardBug,
-	CardCopyRight,
-	CardBanner,
-} from './Card'
+import { CardIntro, CardInfo, CardRoadmap, CardExtendInfo, CardCopyRight, CardBanner } from './Card'
 import Percent from 'assets/percent.jpg'
-import BannerMobile from 'assets/banner-mobile.png'
-import Banner from 'assets/banner.png'
+// import BannerMobile from 'assets/banner-mobile.png'
+// import Banner from 'assets/banner.png'
 import './Homepage.css'
 
 export const HomePage = () => {
@@ -19,10 +10,8 @@ export const HomePage = () => {
 		<div className='home-page'>
 			<CardIntro />
 			{/* <CardSale /> */}
-			<CardBanner desktopView={Banner} mobileView={BannerMobile} />
+			{/* <CardBanner desktopView={Banner} mobileView={BannerMobile} /> */}
 			<CardInfo />
-			<CardBug />
-			<CardSecureInfo />
 			<CardRoadmap />
 			<CardExtendInfo />
 			{/* <CardChart />
