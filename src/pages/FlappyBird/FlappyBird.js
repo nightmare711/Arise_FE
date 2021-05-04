5626d8505ac6d0812e9701d8faed65f4cb5201f3
Night mare <hnbtranspkt@gmail.com>
2021-05-04 23:48:55 +0700

    Ui

--- src/pages/FlappyBird/FlappyBird.js
+++ src/pages/FlappyBird/FlappyBird.js
@@ -3,7 +3,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStopwatch } from '@fortawesome/free-solid-svg-icons'
 import { faPagelines } from '@fortawesome/free-brands-svg-icons'
 import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'
+import PrizesIcon from 'assets/ari-bird/PrizeIcon.jpg'
+import LogoImg from 'assets/logo.png'
+import BgText from 'assets/bg-text.png'
+import RankImg from 'assets/rank-img.png'
 import './FlappyBird.css'
+const tempData = [
+	{
+		address: '0x310377bD2ecD9a800fAEd591E666F9f07382d41d',
+		highest_score: 200,
+	},
+	{
+		address: '0x310377bD2ecD9a800fAEd591E666F9f07382d41d',
+		highest_score: 200,
+	},
+	{
+		address: '0x310377bD2ecD9a800fAEd591E666F9f07382d41d',
+		highest_score: 200,
+	},
+	{
+		address: '0x310377bD2ecD9a800fAEd591E666F9f07382d41d',
+		highest_score: 200,
+	},
+	{
+		address: '0x310377bD2ecD9a800fAEd591E666F9f07382d41d',
+		highest_score: 200,
+	},
+]
 
 export const FlappyBird = () => {
 	return (
@@ -44,7 +70,87 @@ export const FlappyBird = () => {
 					<FontAwesomeIcon icon={faPagelines} className='icon icon-right' />
 				</div>
 			</div>
-			<div className='box box-2'>{/* <img src={} /> */}</div>
+			<div className='box box-2'>
+				<div className='account-score'>
+					<div className='container-logo'>
+						<FontAwesomeIcon icon={faPagelines} className='icon icon-left' />
+						<div className='container-img'>
+							<img src={LogoImg} alt='logo' />
+						</div>
+						<FontAwesomeIcon icon={faPagelines} className='icon icon-right' />
+					</div>
+					<div className='score'>
+						<div className='bg-with-text'>
+							<img src={BgText} alt='Background text' />
+							<span className='txt'>Your Score</span>
+						</div>
+					</div>
+				</div>
+				<div className='header-below'>
+					<div className='bg-with-text'>
+						<img src={BgText} alt='Background text' />
+						<span className='txt'>Team rank</span>
+					</div>
+				</div>
+			</div>
+			<div className='box box-3'>
+				<div className='box-3-content'>
+					<div className='rank-score'>
+						<img src={RankImg} alt='rank' />
+						<div className='score'>
+							<span className='value'>
+								<b>$373,685,905</b> <br /> Score
+							</span>
+							<span className='value'>
+								<b>$373,685,905</b> <br /> Score
+							</span>
+							<span className='value'>
+								<b>$373,685,905</b> <br /> Score
+							</span>
+						</div>
+					</div>
+					<div className='table-score'>
+						<div className='table-header'>
+							<h1>Top Score</h1>
+							<span className='extra'>Since start of competition</span>
+						</div>
+						<table>
+							{/* <thead>
+								<tr></tr>
+								<tr></tr>
+								<tr></tr>
+								<tr></tr>
+							</thead> */}
+							<tbody>
+								{tempData.map((item, index) => (
+									<tr>
+										<td className='rank'>
+											{'#'}
+											{index}
+										</td>
+										<td className='score-value'>{item.highest_score}</td>
+										<td className='address'>
+											{item.address.substr(0, 4) +
+												'...' +
+												item.address.substr(item.address.length - 4, item.address.length)}
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div className='header-below'>
+					<img src={PrizesIcon} alt='Prizes Icon' />
+					<div className='bg-with-text'>
+						<img src={BgText} alt='Background text' />
+						<span className='txt'>Team rank</span>
+					</div>
+				</div>
+			</div>
+			<div className='box box-4'>
+				
+			</div>
 		</div>
 	)
 }
