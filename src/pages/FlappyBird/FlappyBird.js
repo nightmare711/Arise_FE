bb796d15471f0c32717136111617bbd43df8ea1d
Night mare <hnbtranspkt@gmail.com>
2021-05-07 13:22:44 +0700

    Change banner

--- src/pages/FlappyBird/FlappyBird.js
+++ src/pages/FlappyBird/FlappyBird.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import ComingSoon from 'assets/ari-bird/coming-soon.jpeg'
+import ComingSoonMobile from 'assets/ari-bird/coming-soon-mobile.jpeg'
 import { Countdown } from 'components'
 import './FlappyBird.css'
 export const FlappyBird = () => {
+	if (window.screen.width > 520) {
+		return (
+			<div className='flappy-bird'>
+				<img src={ComingSoon} alt='Coming soon' />
+				<div className='count-down'>
+					<Countdown />
+				</div>
+			</div>
+		)
+	}
 	return (
 		<div className='flappy-bird'>
-			<img src={ComingSoon} alt='Coming soon' />
+			<img src={ComingSoonMobile} alt='Coming soon' />
 			<div className='count-down'>
 				<Countdown />
 			</div>
